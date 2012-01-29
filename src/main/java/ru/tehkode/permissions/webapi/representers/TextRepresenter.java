e9d1e074b354e4d0c4dfe0617f4eea1854e5a94e
t3hk0d3 <clouster@yandex.ru>
2012-01-29 09:43:52 +0400

    Removed debug output

--- src/main/java/ru/tehkode/permissions/webapi/representers/TextRepresenter.java
+++ src/main/java/ru/tehkode/permissions/webapi/representers/TextRepresenter.java
@@ -3,9 +3,7 @@ package ru.tehkode.permissions.webapi.representers;
 public class TextRepresenter extends StringRepresenter {
 
 	@Override
-	public String representToString(String mimeType, Object obj) {
-		System.out.println("REPRESENTING " + obj.toString());
-	
+	public String representToString(String mimeType, Object obj) {	
 		return obj.toString();
 	}
 }
